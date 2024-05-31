ef107aebe792f447a6d2c6572ef8f38ae912ca6b
4dTailor <91608035+4dTailor@users.noreply.github.com>
2024-05-31 21:57:20 +1000

    got the door working
    it took way too long to get it to work

--- Assets/Denniel K/Scripts/PlayerTeleport.cs
+++ Assets/Denniel K/Scripts/PlayerTeleport.cs
@@ -2,31 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportStand: MonoBehaviour
+public class PlayerTeleport : MonoBehaviour
 {
     public GameObject Player;
-    public GameObject TeleportTarget;
 
-    private void OnTriggerEnter(Collider other)
+    // Start is called before the first frame update
+    void Start()
     {
-        Player.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 200, other.transform.position.z);
-        if (other.transform.tag == "Player")
-        {
-            
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        OnButtonPress();    
+        OnButtonPress();
     }
 
     private void OnButtonPress()
-    {   
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 1, Player.transform.position.z);
         }
     }
-}  
+}
